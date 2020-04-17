1066f9e7b02c7603cb9cfc45c39b4227baf2c8d3
Mavis Chan <mavis.chan@oocl.com>
2020-04-17 15:02:18 +0800

    [Mavis] - Edit configs and Add ParkingBoy

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/ParkingBoyService.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.ParkingBoy;
+import com.thoughtworks.springbootemployee.repository.ParkingBoyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ParkingBoyService {
+    private ParkingBoyRepository parkingboyRepository;
+
+    public List<ParkingBoy> getAll() {
+        return parkingboyRepository.findAll();
+    }
+
+    public ParkingBoy createParkingBoy(ParkingBoy parkingboy) {
+        return parkingboyRepository.save(parkingboy);
+    }
+}
