8ff8a98fb614f9a92baeabc50be911217b0ca35b
Mavis Chan <mavis.chan@oocl.com>
2020-04-17 16:24:22 +0800

    [Mavis] - Added @Autowired for ParkingBoyController

--- src/main/java/com/thoughtworks/springbootemployee/repository/ParkingBoyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/ParkingBoyRepository.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.springbootemployee.repository;
 
-import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.model.ParkingBoy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
