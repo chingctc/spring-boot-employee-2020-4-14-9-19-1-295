27740adb06cf3f62f46d216253af1746c3678cbc
Mavis Chan <mavis.chan@oocl.com>
2020-04-20 09:05:59 +0800

    [Mavis] - Passed should_get_employee_by_gender & should_update_employee Service Tests

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -16,5 +17,18 @@ public class EmployeeServiceTest {
         EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
         List<Employee> expected = new ArrayList<>();
         when(repository.findAll()).thenReturn(expected);
+    }
+
+    @Test
+    public void should_get_employee_by_gender() throws Exception {
+        EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
+        List<Employee> expected = new ArrayList<>();
+        when(repository.findAllByGender("Male")).thenReturn(expected);
+    }
+
+    @Test
+    public void should_update_employee() throws Exception {
+        EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
+        EmployeeService service = new EmployeeService(repository);
     }
 }
